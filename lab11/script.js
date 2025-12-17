c0c941fce37f88771f55da5f4d57adf6bcef1531
Jhay Baguisa <jhaybaguisa257@gmail.com>
2025-12-17 15:16:30 +0800

    Initial commit

---
+++ lab11/script.js
@@ -0,0 +1,18 @@
+// Variable declaration using let
+let number1 = 10;
+let number2 = 5;
+
+// Simple console output
+console.log("JavaScript is running successfully!");
+
+// Math operation
+let result = number1 + number2;
+
+// Output result to console
+console.log(`The sum of ${number1} and ${number2} is ${result}`);
+
+// Optional: function for button click
+function showMessage() {
+  alert(`The sum of ${number1} and ${number2} is ${result}`);
+}
+    
---
+++ lab12/dom.js
@@ -0,0 +1,4 @@
+document.getElementById("change-btn").addEventListener("click", function() {
+  const heading = document.getElementById("page-title");
+  heading.innerText = "The Title Has Been Changed!";
+});
